e9a41ab09975a405ceb29d14188abfde7cee3379
carlosneto062711-debug <carlosneto062711@gmail.com>
2026-06-25 22:57:30 -0300

    Update version to 111 and fix JS closure
    Updated version to 111 and fixed JavaScript closure issue.

--- mata-mata.js
+++ mata-mata.js
@@ -1,4 +1,4 @@
-// VERSÃO 110 - Status ao vivo no mata-mata
+// VERSÃO 111 - Corrige fechamento JS
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
@@ -865,12 +865,12 @@ function iniciarContagem() {
   window.contagemMataMataLigada = true;
 
   setInterval(() => {
-  carregarProximoJogo();
+    carregarProximoJogo();
 
-  if (!window.usuarioSalvandoPalpiteMataMata && !window.usuarioInteragindoMataMata) {
-    carregarChaveamento();
-  }
-}, 1000);
+    if (!window.usuarioSalvandoPalpiteMataMata && !window.usuarioInteragindoMataMata) {
+      carregarChaveamento();
+    }
+  }, 1000);
 }
 
 function configurarLoginMataMata() {
